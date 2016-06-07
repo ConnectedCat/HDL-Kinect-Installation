3c991297a84913828fef9a07d155e01f8d22e44d
Maxim Safioulline <maxim@connectedcatmedia.com>
2016-06-07 19:57:45 -0400

    Rebuilt the stream from the camera to set the background pixels to be transparent and set the body pixels to a color snowcrash.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,20 +1,35 @@
 #include "ofApp.h"
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofSetWindowShape(width, height);
+	ofSetWindowShape(APP_WIDTH, APP_HEIGHT);
+	ofEnableAlphaBlending();
+
 	ofSetFrameRate(20);
 	for (int i = 0; i<PNUM; i++) {
-		positions[i].set(ofRandom(-340, ofGetWidth() + 340), ofGetHeight() - 63, ofRandom(-300, -100));
+		positions[i].set(ofRandom(-340, APP_WIDTH + 340), APP_HEIGHT - 63, ofRandom(-300, -100));
 		Pigeon newPigeon;
 		newPigeon.setup(positions[i]);
 		myPigeons.push_back(newPigeon);
 	}
 
 	bFullscreen = false;
+	numBodiesTracked = 0;
+	bHaveAllStreams = false;
 
 	kinect.open();
+	kinect.initDepthSource();
 	kinect.initBodySource();
 	kinect.initBodyIndexSource();
+
+	if (kinect.getSensor()->get_CoordinateMapper(&coordinateMapper) < 0) {
+		ofLogError() << "Could not acquire CoordinateMapper!";
+	}
+
+	bodyIndexImage.allocate(DEPTH_WIDTH, DEPTH_HEIGHT, OF_IMAGE_COLOR_ALPHA);
+
+	colorCoords.resize(DEPTH_WIDTH * DEPTH_HEIGHT);
+	backgroundColor.set(255, 0); //fully transparent background
 }
 
 //--------------------------------------------------------------
@@ -25,11 +40,26 @@ void ofApp::update(){
 	else {
 		ofShowCursor();
 	}
+
 	kinect.update();
 
+	auto& depthPix = kinect.getDepthSource()->getPixels();
+	auto& bodyIndexPix = kinect.getBodyIndexSource()->getPixels();
+	// Make sure there's some data here, otherwise the cam probably isn't ready yet
+	if (!depthPix.size() || !bodyIndexPix.size()) {
+		bHaveAllStreams = false;
+		return;
+	}
+	else {
+		bHaveAllStreams = true;
+	}
+
+	numBodiesTracked = 0;
 	bodies = kinect.getBodySource()->getBodies();
 	for (auto body : bodies) {
 		if (body.tracked) {
+			numBodiesTracked++;
+
 			leftHandPos = body.joints[JointType_HandLeft].getPosition();
 			rightHandPos = body.joints[JointType_HandRight].getPosition();
 			leftFootPos = body.joints[JointType_FootLeft].getPosition();
@@ -65,25 +95,44 @@ void ofApp::update(){
 						if (myPigeons[i].state == "walking") {
 							myPigeons[i].fly();
 						}
-						//if (myPigeons[i].state == "flying" && myPigeons[i].position.y < ofGetHeight() / 2) {
-						//	myPigeons[i].turn();
-						//	if (myPigeons[i].canShit) {
-						//		Diamond diamond = myPigeons[i].shitDiamond();
-						//		diamonds.push_back(diamond);
-						//	}
-						//}
+						if (myPigeons[i].state == "flying" && myPigeons[i].position.y < ofGetHeight() / 2) {
+							myPigeons[i].turn();
+							if (myPigeons[i].canShit) {
+								Diamond diamond = myPigeons[i].shitDiamond();
+								diamonds.push_back(diamond);
+							}
+						}
 					}
 
-					//if ((xdistancelf <= 20 && ydistancelf <= 40) || (xdistancerf <= 20 && ydistancerf <= 40)) {
-					//	myPigeons[i].splat();
-					//	Pigeon corpse = myPigeons[i];
-					//	myPigeons.erase(myPigeons.begin() + i);
-					//	myPigeons.push_front(corpse);
-					//}
-				}
+					if ((xdistancelf <= 20 && ydistancelf <= 40) || (xdistancerf <= 20 && ydistancerf <= 40)) {
+						myPigeons[i].splat();
+						Pigeon corpse = myPigeons[i];
+						myPigeons.erase(myPigeons.begin() + i);
+						myPigeons.push_front(corpse);
+					}
+				} // end if pigeon isn't dead
+			}//end for every pigeon
+		}//end if body tracked
+	}// end for every body
+
+	coordinateMapper->MapDepthFrameToColorSpace(DEPTH_SIZE, (UINT16*)depthPix.getPixels(), DEPTH_SIZE, (ColorSpacePoint*)colorCoords.data());
+
+	for (int y = 0; y < DEPTH_HEIGHT; y++) {
+		for (int x = 0; x < DEPTH_WIDTH; x++) {
+			int index = (y * DEPTH_WIDTH) + x;
+
+			bodyIndexImage.setColor(x, y, backgroundColor);
+
+			float val = bodyIndexPix[index];
+			if (val >= bodies.size()) {
+				continue;
 			}
+			bodyColor.set(ofRandom(255), ofRandom(255), ofRandom(255), 255); //make it into a snowcrash
+			bodyIndexImage.setColor(x, y, bodyColor);
 		}
 	}
+
+	bodyIndexImage.update();
 
 	ofBackground(155);
 	for (int i = 0; i<PNUM; i++) {
@@ -103,12 +152,17 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	for (int i = 0; i<PNUM; i++) {
+		myPigeons[i].display();
+	}
 	if (!bFullscreen) {
-		kinect.getBodyIndexSource()->draw(0, 0, width, height);
+		//kinect.getBodyIndexSource()->draw(0, 0, width, height);
 		//kinect.getBodySource()->drawProjected(0, 0, width, height, ofxKFW2::ProjectionCoordinates::DepthCamera);
+		bodyIndexImage.draw(0, 0, APP_WIDTH, APP_HEIGHT);
 	}
 	else {
-		kinect.getBodyIndexSource()->draw(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
+		//kinect.getBodyIndexSource()->draw(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
+		bodyIndexImage.draw(0, 0);
 	}
 
 	if (diamonds.size() > 0) {
@@ -116,9 +170,7 @@ void ofApp::draw(){
 			diamonds[i].display();
 		}
 	}
-	for (int i = 0; i<PNUM; i++) {
-		myPigeons[i].display();
-	}
+
 
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -5,7 +5,12 @@
 #include "Pigeon.h"
 #include "Diamond.h"
 
-#define PNUM 17                                          
+#define PNUM 17
+#define DEPTH_WIDTH 512
+#define DEPTH_HEIGHT 424
+#define DEPTH_SIZE DEPTH_WIDTH * DEPTH_HEIGHT
+#define APP_WIDTH DEPTH_WIDTH * 2
+#define APP_HEIGHT DEPTH_HEIGHT * 2
 
 class ofApp : public ofBaseApp{
 
@@ -25,25 +30,33 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
-		ofxKFW2::Device kinect;
 
 		int width = 1024;
 		int height = 848;
 
 		bool bFullscreen;
 
+		deque<Pigeon> myPigeons;
+		ofVec3f positions[PNUM];
+		vector<Diamond> diamonds;
+
+		ofxKFW2::Device kinect;
+
+		vector<ofxKinectForWindows2::Data::Body> bodies;
+		//string posLOut, posROut, posBOut;
+		ICoordinateMapper* coordinateMapper;
+
+		ofImage bodyIndexImage;
+
+		ofColor backgroundColor;
+		ofColor bodyColor;
+
+		vector<ofVec2f> colorCoords;
+		int numBodiesTracked;
+		bool bHaveAllStreams;
+
 		ofVec3f rightHandPos;
 		ofVec3f leftHandPos;
 		ofVec3f leftFootPos;
 		ofVec3f rightFootPos;
-
-		vector<ofxKinectForWindows2::Data::Body> bodies;
-		string posLOut, posROut, posBOut;
-
-		deque<Pigeon> myPigeons;
-		//Pigeon myPigeons[PNUM];
-		ofVec3f positions[PNUM];
-
-		vector<Diamond> diamonds;
 };
